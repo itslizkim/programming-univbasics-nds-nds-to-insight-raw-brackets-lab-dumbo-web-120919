11ce8aaa11360b2be9822e82ece01b719f178491
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 21:24:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -21,7 +21,7 @@ def directors_totals(nds)
     i += 1
   end      # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
+  p total#
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
